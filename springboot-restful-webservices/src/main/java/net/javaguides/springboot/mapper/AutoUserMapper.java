33bfdc756bc3e04e8516622083a321e21161c22e
lokasis <lokasisghorai@gmail.com>
2024-12-09 23:50:41 +0530

    Changes for Java 21 Upgrade

--- springboot-restful-webservices/src/main/java/net/javaguides/springboot/mapper/AutoUserMapper.java
+++ springboot-restful-webservices/src/main/java/net/javaguides/springboot/mapper/AutoUserMapper.java
@@ -5,7 +5,7 @@ import net.javaguides.springboot.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface AutoUserMapper {
 
     AutoUserMapper MAPPER = Mappers.getMapper(AutoUserMapper.class);
